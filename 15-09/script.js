4705565fe505f0f29703dc7f80d4d008a3287794
leevi <levi@gmail.com>
2026-09-15 11:38:44 -0300

    adicinada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,38 @@
+// let variante = "Peter Parker";
+// let number = "99999999"
+// let number2 = "999999999999"
+// let idade = 17;
+
+
+// idade = 20;
+// idade = 21;
+
+
+// console.log("O miranha é o " + variante + "e ele tem " + idade + " anos");
+
+let nome = "michael";
+let sobrenome = "myers";
+let idade = "68";
+let curso = "serial killer";
+
+// console.log("Olá, meu nome é " + nome + " " + sobrenome + " e tenho " + idade + " anos de idade. " + "Faço o ~çlkjhcurso de " + curso )
+ 
+
+
+// let nome = "oswaldo"; //string
+// let peso = 90; //int
+// let altura = 1.99; //float
+// let funcionario = false;//bool - true == verdadeiro ou false == falso
+
+// let cidade =null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+console.log("A variavel nome tem " + nome + " " + "e é do tipo " + typeof nome)
+console.log("A variavel sobrenome tem " + sobrenome + " " + "e é do tipo " + typeof nome)
+console.log("A variavel idade tem " + idade + " " + "e é do tipo " + typeof nome)
+console.log("A variavel curso tem " + curso + " " + "e é do tipo " + typeof nome)
+
