1385d127e013d6fc1cc324bc3509d3a1df42e580
leevi <levi@gmail.com>
2026-09-16 11:56:33 -0300

    criação da pasta 16-09 e estrutura condicional

---
+++ 16-09/script.js
@@ -0,0 +1,103 @@
+// operações aritimedicos
+// + soma
+// - subtração
+// * multiplicação
+// / divisão
+// % sobra da divisão
+
+// let n1 = 3;
+// let n2 = 9;
+
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n1 / n2);
+// console.log(n1 % n2);
+// console.log((n1 - n2) * 10);
+// console.log("palavra" + n1);
+
+// crie 3 variáveis, cada uma contendo um número,
+//  / some as 3 e tire a média, exiba o resultado no console
+
+// let hj = 67;
+// let jh = 6767;
+// let kj = 76;
+
+// console.log((hj + jh + kj) / 3);
+
+// console.log("a média de " + " + " + hj + " + " + jh + " + " + kj + " + " + "é " + (hj + jh + kj) / 3);
+
+// > maior que 
+// < menor que
+// == igual
+// >= maior ou igual
+// <= menor ou igual
+// != é diferente
+
+// let idade = 18;
+
+// console.log(idade > 18);
+// console.log(idade == 18);
+// console.log(idade >= 18);
+// console.log(idade <= 18);
+// console.log(idade < 18);
+// console.log(idade != 18);
+
+// console.log(idade === 18);
+
+// let idade = 18;
+// let habilitado = true;
+// console.log(idade <= 18 && habilitado == true);
+// V || V =
+// V || F =
+// F || V =
+// F || F =
+
+// ! operador NOT "inverter"
+// console.log(!habilitado);
+
+//se idade for igual a 18 anos
+    //faça isso
+
+//se não
+    //faça isso
+//
+// let idade = 15;
+
+// if(idade >=18) {
+// if(idade > 17) 
+//     console.log("maior de idade");
+// } else {
+//     console.log("menor de idade");
+// }
+
+
+
+// let michael = 1234;
+
+// if(michael == 1234) {
+//     console.log ("acesso liberto")
+// } else {
+//     console.log("acesso negante")
+// }
+
+
+// let bad = 7;
+
+// if(bad >= 6) {
+//     console.log ("passarte delo anio")
+// }   else {
+//     console.log ("repeteste")
+// }
+
+
+let compra = 208
+
+if(compra > 500) {
+    console.log (compra - 20)
+}
+
+
+
+
+
