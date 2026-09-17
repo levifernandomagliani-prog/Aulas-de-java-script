3a53814d67f323b31294621b5c6477ad5bb13914
leevi <levi@gmail.com>
2026-09-17 11:58:10 -0300

    exercicios aula 17 finalizados

---
+++ 17-09/script.js
@@ -0,0 +1,119 @@
+// let nota = 7;
+
+// if(nota < 5) {
+//     console.log("henrique reprovado");
+// } else if (nota < 7) {
+//     console.log("henrique de recuperação");
+// }else {
+//     console.log ("henrique aprovado")
+// }
+
+
+
+
+// Exercicio
+let temperatufa = 40; 
+
+if (temperatufa > 30) {
+    console.log("tá carlor")
+} else if (temperatufa >= 20 && temperatufa < 30 ) {
+    console.log("tá bom")
+} else {
+     console.log("eatar frio")
+} 
+
+// exercicio 2
+
+let speed = 700; 
+
+if (speed > 70) {
+    console.log("rapido")
+}else if (speed >= 50 && speed < 60 ) {
+    console.log("andante")
+} else {
+     console.log("lesma")
+} 
+
+// exercicio 3
+
+let estoque = 15; 
+
+if (estoque == 0) {
+    console.log("estoque zerado")
+}else if (estoque <= 30 && estoque >= 10) {
+    console.log("estoque estoque baixo")
+} else{
+    console.log("normal")
+} 
+
+// exercicio 4
+
+let usuário = "admin";
+let senha = 1234;
+
+if (usuário == "admin" && senha == 1234) {
+    console.log("MISSION PASSED")
+} else {
+    console.log("SE FERROU") 
+}
+
+
+//exercicio 5
+
+let hora = 4
+
+if (hora >= 0 && hora <= 11) {
+    console.log("boa madrugada/ bom dia")
+} else if (hora > 11 && hora < 18) {
+    console.log("boa tarde")
+} else if(hora >= 18 && hora <= 23) {
+    console.log("boa noite cindela")
+}
+
+
+// exercicio 6
+
+let nota = 5
+
+if(nota == 1) {
+    console.log("lixo")
+} else if(nota == 2) {
+    console.log("ainda ruim")
+} else if(nota == 3) {
+    console.log("decente")
+} else if(nota == 4) {
+    console.log("dahora")
+} else if(nota == 5) {
+    console.log("muito toper")
+}
+
+// exercicio 7
+
+let tipoingresso = "vip";
+
+if (tipoingresso == "vip") {
+    console.log("80 reais")
+} else if(tipoingresso == "meia") {
+    console.log("20 reais")
+} else if(tipoingresso == "inteira") {
+    console.log("40")
+}else{
+    console.log("ingresso inválido")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
