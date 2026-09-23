eea7981e0eee7bc0519144c9866fbf198445ef75
leevi <levi@gmail.com>
2026-09-23 11:54:17 -0300

    aula 23/09 - estrutura de repetição

---
+++ 23-09/script.js
@@ -0,0 +1,157 @@
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
+// console.log("10");
+
+// FOR
+// for (variavel; condicao; incremento) {
+
+// }
+
+
+//INCREMENTO
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
+
+// //DECREMENTO
+// for (let index = 10; index >= 0; index--) {
+//     console.log(index);
+    
+// }
+
+// //INCREMENTO PERSONALIZADO
+// for (let index = 10; index >= 0; index-=2) {
+//     console.log(index);
+    
+// }
+
+// for (let index = 10; index >= 0; index-=5) {
+    // console.log(index);
+    
+// }
+
+//exercicios
+
+// for (let index = 1; index <= 20; index++) {
+//     console.log(index);
+    
+// }
+
+// for (let index = 5; index <= 50; index+=5) {
+//     console.log(index);
+    
+// }
+
+
+// for (let index = 10; index >= 0; index--) {
+    // console.log(index);
+    
+// } 
+// console.log("contagem finish");
+
+
+
+
+
+// --------- WHILE
+// let contador = 0;
+// while (contador <= 10) {
+
+    // console.log(contador);
+    
+
+    // contador++;
+// }
+
+//exercicio 1
+// let contador = 10;
+// while (contador <= 20) {
+
+//     console.log(contador);
+    
+
+//     contador++;
+// }
+
+//exercicio 2
+// let contador = 2;
+// while (contador <= 20) {
+
+    // console.log(contador);
+    
+
+    // contador+=2;
+// }
+
+// ---------- DO...WHILE
+// let n = 10;
+// while (n < 5) {
+//     console.log("WHILE");
+
+// }
+
+// do {
+//     console.log("DO...WHILE");
+
+// }while (n <5);
+
+
+// let contador = 0;
+// do {
+    // console.log(contador);
+
+    // contador++;
+
+// } while (contador <= 10);
+
+
+// exercicio 1
+// let contador = 10;
+// do {
+//     console.log(contador);
+    
+//     contador++;
+
+// } while (contador <= 100);
+
+//exercicio 2
+// let contador = 10;
+// do {
+//     console.log(contador);
+    
+//     contador--;
+
+// } while (contador >= 1);
+
+
+//--------- ARRAY (LISTA)---------
+let nome1 = "levi";
+let nome2 = "duda";
+let nome3 = "gustavo";
+let nome4 = "bernardo";
+
+//              0       1        2           3
+let nomes = ["levi", "duda", "gustavo", "bernardo"];
+
+console.log(nomes);//mostra a lista completa na mesma linha
+
+console.log(nomes[1]);//mostra o item da posição mencionada entre colchetes
+
+nome4 = "ana";
+nomes[3] = "ana";  //altero o valor na posicão especificada
+
+console.log(nomes.length);//mostra o tamanho do array
+
+
+
+
+
+
